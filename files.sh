3ef9ed6902fcb878264bf543bba945cf0a504d56
FredyM <saiberz@hotmail.com>
2014-05-22 16:20:34 -0500

    second commit

--- files.sh
+++ files.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 
-echo "hi world"
-echo "enconre"
-wget http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+echo  '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>'
+echo  '<script src="https://cdn.firebase.com/js/simple-login/1.4.1/firebase-simple-login.js"></script>'
+echo  '<script src="https://cdn.firebase.com/js/client/1.0.15/firebase.js"></script>'
+# wget http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
