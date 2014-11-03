4b770cf33f0d426f217583caa90da8ea3b7ac9b1
Fredy Morales <saiberz@hotmail.com>
2014-11-03 15:14:11 -0500

    * README.md: Modified

--- src/clojuredep/core.clj
+++ src/clojuredep/core.clj
@@ -5,25 +5,12 @@
    [clostache.parser :as clostache]
    [compojure.route :as route]))
 
-(defn read-template [name]
-  (slurp
-   (clojure.java.io/resource
-    (str "templates/" name ".mustache"))))
-
-(defn render-template [template-file params]
-  (clostache/render
-   (read-template template-file) params))
-
-(defn index []
-  (render-template "index" {:name "Oracle"}))
-
+(defroutes main-routes
+  (GET "/" [] "Welcome world")
+  (GET "/api" [] "{\"objects\": [{\"a\":1, \"b\":2}, {\"a\":4,\"b\":9}]}"))
 
 (defn -main []
   (let [port
         (Integer/parseInt
-         (get (System/getenv) "PORT" "5000"))]
+         (get (System/getenv) "PORT" "8080"))]
     (jetty/run-jetty main-routes {:port port})))
-
-(defroutes main-routes
-  (GET "/api" [] "{\"objects\": [{\"a\":1, \"b\":2}, {\"a\":4,\"b\":9}]}")
-  (GET "/home" [] (index)))
